5d309fd8804082ff9b6fc47be72da1870cc038b8
Bianca Burzo <bburzo18@gmail.com>
2021-10-19 17:57:01 +0300

    Hausaudgabe2_MAP

---
+++ test/java/com/company/BenutzerTest.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BenutzerTest {
+
+    @Test
+    void kalkuliereZeit() {
+
+    }
+
+    @Test
+    void testKalkuliereZeit() {
+    }
+
+    @Test
+    void kalkuliereDurschnittszeit() {
+    }
+}
