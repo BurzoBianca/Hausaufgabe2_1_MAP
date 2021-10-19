1972a0fffc824a2ac7437c149bfa749afebaacf4
Bianca Burzo <bburzo18@gmail.com>
2021-10-19 21:44:36 +0300

    Hausaudgabe2_MAP

--- test/java/com/company/BenutzerTest.java
+++ test/java/com/company/BenutzerTest.java
@@ -1,5 +1,9 @@
 package com.company;
+import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -11,13 +15,37 @@ class BenutzerTest {
     @Test
     void kalkuliereZeit() {
 
+        Sport basketball = new Basketball();
+        Sport fussball = new Fussball();
+        Sport hindernislauf = new Hindernislauf();
+        Sport hochsprung = new Hochsprung();
+
+        Benutzer benutzer = new Benutzer("Bianca", "Burzo", Arrays.asList(basketball, fussball, hindernislauf, hochsprung));
+        assertEquals(benutzer.kalkuliereZeit(), benutzer.kalkuliereZeit1(basketball) + benutzer.kalkuliereZeit1(fussball) + benutzer.kalkuliereZeit1(hindernislauf) + benutzer.kalkuliereZeit1(hochsprung));
     }
 
     @Test
-    void testKalkuliereZeit() {
+    void kalkuliereZeit1() {
+        Sport basketball = new Basketball();
+        Sport fussball = new Fussball();
+        Sport hindernislauf = new Hindernislauf();
+        Sport hochsprung = new Hochsprung();
+        Benutzer benutzer = new Benutzer("Bianca", "Burzo", Arrays.asList(basketball, fussball, hindernislauf, hochsprung));
+
+        assertEquals(benutzer.kalkuliereZeit1(basketball), 55);
+        assertEquals(benutzer.kalkuliereZeit1(fussball), 65);
+        assertEquals(benutzer.kalkuliereZeit1(hindernislauf), 30);
+        assertEquals(benutzer.kalkuliereZeit1(hochsprung), 20);
     }
 
     @Test
     void kalkuliereDurschnittszeit() {
+        Sport basketball = new Basketball();
+        Sport fussball = new Fussball();
+        Sport hindernislauf = new Hindernislauf();
+        Sport hochsprung = new Hochsprung();
+
+        Benutzer benutzer = new Benutzer("Smith", "Bob", Arrays.asList(basketball, fussball, hindernislauf, hochsprung));
+        assertEquals(benutzer.kalkuliereDurschnittszeit(), benutzer.kalkuliereZeit() / 4);
     }
 }
